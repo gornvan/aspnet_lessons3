3f11e93936e98a7eb3962f9505738b5b3bda8798
gornvan <grobelvan@gmail.com>
2024-08-12 20:12:59 +0200

    create a simple example of a console project for Dumpin Demonstration

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// to dump, install dotnet dump tool
+
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+var a = 33;
+
+var b = a * 2;
+
+Test.DoStuff(b);
+
+
+Console.WriteLine(b);
---
+++ ConsoleApp1/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal static class Test
+    {
+        static public void DoStuff(int value)
+        {
+            value += 40;
+            Console.WriteLine(value);
+
+            // to see this place in the dump debuggin, open the dump in IDE
+            Console.ReadLine();
+        }
+    }
+}
