92d01ab8a0c62e3353b332c3e28a4ab4a3c99a09
gornvan <grobelvan@gmail.com>
2024-08-14 19:00:40 +0200

    reorganize the project into lesson1&2-gitBasics, then copy it to lesson3-xunit

---
+++ lesson3-xUnit/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// to dump, install dotnet dump tool
+
+using ConsoleApp1;
+using ServerPerformance;
+
+Console.WriteLine("Hello, World!");
+
+var a = 33;
+
+var b = a * 2;
+
+Test.DoStuff(b);
+
+
+Console.WriteLine(b);
+
+while (true)
+{
+    var stats = PerformanceStatsProvider.GetPerformanceStats();
+    Console.WriteLine("Total CPU Utilization: {0}%", stats.TotalCpuUtilizationPercentage);
+    Console.WriteLine("Occupied Memory: {0}%", stats.OccupiedMemoryPercentage);
+    Console.WriteLine("Total Running Processes: {0}", stats.TotalRunningProcessesCount);
+    await Task.Delay(1000);
+}
