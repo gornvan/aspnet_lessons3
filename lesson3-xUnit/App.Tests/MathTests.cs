6ff824f074e4ecb298fadc6b596b4df7bd3e10f5
gornvan <grobelvan@gmail.com>
2024-08-14 19:57:41 +0200

    lesson3: create an example of multi-input test

---
+++ lesson3-xUnit/App.Tests/MathTests.cs
@@ -0,0 +1,21 @@
+﻿namespace App.Tests;
+
+public class MathTests
+{
+    [Theory]
+    [InlineData(10, 3, 1000)]
+    [InlineData(-1, 3, -1)]
+    [InlineData(0, 3, 0)]
+    [InlineData(0.5, 3.0, 125)]
+    public void Power(double baseN, double powerN, double expected)
+    {
+        // arrange is done
+
+        // act
+        var result = Math.Pow(baseN, powerN);
+
+        // assert
+        Assert.Equal(expected, result);
+    }
+}
+
