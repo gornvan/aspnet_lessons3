cbaa3e4d6f7b59b373d1a0c50dded0604812785f
gornvan <grobelvan@gmail.com>
2024-08-21 20:54:08 +0200

    lesson5: accept only GET requests on Home Controller

--- lesson5_MVC_Routing/Controllers/HomeController.cs
+++ lesson5_MVC_Routing/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ public class HomeController
     (ILogger<HomeController> logger)
     : Controller
 {
+    [HttpGet]
     public IActionResult Index()
     {
         var incominConnection = HttpContext.Connection;
@@ -18,12 +19,14 @@ public class HomeController
         return View(incomingAddress);
     }
 
+    [HttpGet]
     public IActionResult Privacy(int id = 0)
     {
         return View(id);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [HttpGet]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
