ce84fc2086fb70cc00f7a43dadfead431afd56ba
gornvan <grobelvan@gmail.com>
2024-09-16 18:20:42 +0200

    copy lesson 5 to lesson 6 - gonna demonstrate an MVC crud

---
+++ lesson6_MVC_CRUD/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lesson5_MVC_Routing.Models;
+
+namespace lesson5_MVC_Routing.Controllers;
+
+public class HomeController
+    (ILogger<HomeController> logger)
+    : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var incominConnection = HttpContext.Connection;
+        var incomingAddress = incominConnection.RemoteIpAddress;
+
+        logger.LogInformation($"[{incomingAddress}] has accessed the Index of Home!");
+
+        return View(incomingAddress);
+    }
+
+    [HttpGet]
+    public IActionResult Privacy(int id = 0)
+    {
+        return View(id);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [HttpGet]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
