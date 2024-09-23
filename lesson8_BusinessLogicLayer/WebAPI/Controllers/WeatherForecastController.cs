1a3eb1081f357cb9968d906772f7cc716ba51938
gornvan <grobelvan@gmail.com>
2024-09-23 20:22:09 +0200

    copy WebApi from lesson 7 to lesson 8

---
+++ lesson8_BusinessLogicLayer/WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,114 @@
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace lesson8_WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Example of a RESTful endpoint allowing to filter, paginate and project the data
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast?> Get(
+            int minTemperatureC,
+            int maxTemperatureC,
+            int pageSize = 5,
+            int pageNumber = 1,
+            [FromQuery] string[]? fields = null)
+        {
+            var unprojectedQuery = Enumerable.Range(1, 1024 * 1024 * 1024 /* Emulating a LARGE source of data which we will not consume in full thanks to Pagination */)
+                .Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                // Always filter BEFORE paging
+                .Where(forecast =>
+                    forecast.TemperatureC >= minTemperatureC
+                    &&
+                    forecast.TemperatureC <= maxTemperatureC)
+                // Always page AFTER filtering
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize);
+
+            var projectedQuery = fields == null
+                ? unprojectedQuery
+                : unprojectedQuery.Select(forecast => MutateObjectToGetProjection(forecast, fields));
+
+            return projectedQuery.ToList();
+        }
+
+        private static T? MutateObjectToGetProjection<T>(T? originalObject, string[] fieldNames) where T: class
+        {
+            if (originalObject == default(T))
+            {
+                return originalObject;
+            }
+
+            var props = originalObject.GetType().GetProperties().Where(p => p.IsPubliclyWritable());
+
+            foreach (var prop in props)
+            {
+                if (!fieldNames.Any(
+                    fn => prop.Name.Equals(fn, StringComparison.OrdinalIgnoreCase))
+                )
+                {
+                    prop.SetValue(originalObject, null);
+                }
+            }
+            return originalObject;
+        }
+
+        /// <summary>
+        /// Allow the client to request the forecase in a certain DATA TYPE via ACCEPT header
+        /// </summary>
+        /// <param name="daysAhead">The day of interest</param>
+        /// <returns></returns>
+        [Produces("application/json", new[] { "text/plain" })]
+        [HttpGet(template: "{daysAhead}", Name = "GetWeathForecastForConcreteDayAhead")]
+        public IActionResult Get(int daysAhead)
+        {
+            var forecast = new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(daysAhead)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            };
+
+            var accept = Request.GetTypedHeaders().Accept;
+            switch (accept[0].MediaType.ToString())
+            {
+                case "application/json":
+                case "*/*":
+                default:
+                    return new JsonResult(forecast);
+
+                case "text/plain":
+                    return Content(
+                        $"""
+                        Date: {forecast.Date};
+                        The temperature will be {forecast.TemperatureC} Celcius;
+                        It will feel {forecast.Summary}.
+                        """
+                        );
+            }
+        }
+    }
+}
---
+++ lesson8_BusinessLogicLayer/WebAPI/Program.cs
@@ -0,0 +1,57 @@
+
+using lesson8_WebApi.Middlewares;
+using System.Reflection;
+
+namespace lesson8_WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddHttpLogging(o => { });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+            });
+
+            var app = builder.Build();
+
+            app.UseHttpLogging();
+
+            ApplyApiVersionRoutePrefix(app);
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.UseResponseCompression();
+            app.Run();
+        }
+
+        private static void ApplyApiVersionRoutePrefix(WebApplication app)
+        {
+            var appVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            var pathPrefix = $"/api/{appVersion}";
+            app.UseMiddleware<GlobalRoutePrefixMiddleware>(pathPrefix);
+            app.UsePathBase(pathPrefix);
+        }
+    }
+}
