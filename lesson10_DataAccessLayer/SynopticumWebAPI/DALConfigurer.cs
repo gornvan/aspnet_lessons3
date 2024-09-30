f39aadf545d6ff497a35abb9cfc8ed13187ff5e1
gornvan <grobelvan@gmail.com>
2024-09-30 20:30:55 +0200

    add DALConfigurer and the settings for connection into appsettings.Development.json

---
+++ lesson10_DataAccessLayer/SynopticumWebAPI/DALConfigurer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SynopticumDAL;
+
+namespace SynopticumWebAPI
+{
+    public static class DALConfigurer
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+
+            var serverVersion = configuration.GetSection("MySql").GetValue<string>("Version");
+
+            services.AddDbContext<SynopticumDbContext>(
+                dbContextOptions => dbContextOptions
+                    .UseMySql(connectionString, ServerVersion.Parse(serverVersion))
+                    // The following three options help with debugging, but should
+                    // be changed or removed for production.
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors()
+            );
+        }
+    }
+}
