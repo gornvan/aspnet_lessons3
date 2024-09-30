516920606ba35ecacaf75b558c43bd3b80660204
gornvan <grobelvan@gmail.com>
2024-09-30 20:10:44 +0200

    Create a project for Data Access Layer, then Scaffold the dbcontext via CLI command: dotnet-ef dbcontext scaffold "server=localhost;user=Synopticum;password=123123;database=synopticum" Pomelo.EntityFrameworkCore.MySql

---
+++ lesson10_DataAccessLayer/SynopticumDAL/SynopticumContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace SynopticumDAL;
+
+public partial class SynopticumContext : DbContext
+{
+    public SynopticumContext()
+    {
+    }
+
+    public SynopticumContext(DbContextOptions<SynopticumContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;user=Synopticum;password=123123;database=synopticum", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb3_bin")
+            .HasCharSet("utf8mb3");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
