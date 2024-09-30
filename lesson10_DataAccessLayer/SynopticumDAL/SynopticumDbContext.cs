b9ecd107e7b8bac9a362b6630d1ab3a338ab6ad1
gornvan <grobelvan@gmail.com>
2024-09-30 20:29:33 +0200

    rename SynopticumContext into SynopticumDbContext

---
+++ lesson10_DataAccessLayer/SynopticumDAL/SynopticumDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SynopticumDAL;
+
+public partial class SynopticumDbContext : DbContext
+{
+    public SynopticumDbContext()
+    {
+    }
+
+    public SynopticumDbContext(DbContextOptions<SynopticumDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql("server=localhost;user=Synopticum;password=123123;database=synopticum",
+            ServerVersion.Parse("8.0.39-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb3_bin")
+            .HasCharSet("utf8mb3");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
