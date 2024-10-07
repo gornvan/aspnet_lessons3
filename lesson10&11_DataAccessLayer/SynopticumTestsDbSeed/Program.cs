17fc272dec0be78bcb02379dda94e085330c3d91
gornvan <grobelvan@gmail.com>
2024-10-07 19:49:14 +0200

    Automate DB Seeding with a separate console program

---
+++ lesson10&11_DataAccessLayer/SynopticumTestsDbSeed/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SynopticumCoreTests;
+using SynopticumDAL;
+using SynopticumDAL.Seed;
+using SynopticumDAL.Services;
+
+namespace SynopticumTestsDbSeed
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var dbContext = DbContextInitializer.Initialize("seedsettings.json");
+
+            dbContext.Database.EnsureCreated();
+
+            var seeder = new SynopticumDbSeed(dbContext);
+            await seeder.Seed();
+
+            Console.WriteLine($@"The DB {dbContext.Database.GetDbConnection().Database} has been seeded successfully!");
+        }
+    }
+}
