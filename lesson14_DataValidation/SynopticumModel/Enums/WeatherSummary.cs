6d5927553c21d7fa1483ccb43242b313d20f6ce9
gornvan <grobelvan@gmail.com>
2024-10-16 21:20:39 +0200

    Create a validator for NewWeatherForecast

--- lesson14_DataValidation/SynopticumModel/Enums/WeatherSummary.cs
+++ lesson14_DataValidation/SynopticumModel/Enums/WeatherSummary.cs
@@ -2,14 +2,15 @@
 {
     public enum WeatherSummary : int
     {
+        Liquifying = 0,
         Freezing = 1,
         Bracing = 2,
         Chilly = 3,
         Cool = 4,
         Mild = 5,
         Warm = 6,
-        Balmy = 7,
-        Hot = 8,
+        Hot = 7,
+        Sweating = 8,
         Sweltering = 9,
         Scorching = 10
     }
