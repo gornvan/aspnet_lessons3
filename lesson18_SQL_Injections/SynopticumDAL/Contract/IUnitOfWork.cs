8cf082410bbe1814e516881c4135ea551632fb3f
gornvan <grobelvan@gmail.com>
2024-10-28 18:31:42 +0100

    copy the solution into Lesson18 for sql injections

---
+++ lesson18_SQL_Injections/SynopticumDAL/Contract/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SynopticumModel.Contract;
+
+namespace SynopticumDAL.Contract
+{
+    public interface IUnitOfWork
+    {
+        IDbContextTransaction BeginTransaction();
+
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntity;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ lesson18_SQL_Injections/SynopticumDAL/EntityConfigurations/CountryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SynopticumModel.Entities;
+
+namespace SynopticumDAL.EntityConfigurations
+{
+    internal class CountryConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+        }
+    }
+}
