91c9c979a721853dc6019c6c99f1670fac0915d9
gornvan <grobelvan@gmail.com>
2024-10-28 19:35:26 +0100

    copy mvc Friends Crud for later demonstration of AntiCSRF

---
+++ lesson18_XSS&CORS/anti-CSRF-in-mvc/FriendsManager.MVC/Controllers/FriendController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FriendsManager.Model;
+using FriendsManager.MVC.ViewModels.Friend;
+
+namespace FriendsManager.MVC.Controllers
+{
+    public class FriendController : Controller
+    {
+        private static List<Friend> _friends = new List<Friend>()
+        {
+            new Friend{FriendID=1, FriendName="John", Place="London"},
+            new Friend{FriendID=2, FriendName="Jan", Place="Poland"},
+            new Friend{FriendID=3, FriendName="Mikalaj", Place="Minsk"},
+        };
+
+        public IActionResult Index() // /friends
+        {
+            return View(_friends);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Friend friend)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(friend);
+            }
+
+            _friends.Add(friend);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var friend = _friends
+                .FirstOrDefault(f => f.FriendID == id);
+
+            if (friend == null)
+            {
+                return NotFound();
+            }
+
+            return View(friend);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Friend friend)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(friend);
+            }
+
+            var existingFriend = _friends
+                .FirstOrDefault(f => f.FriendID == friend.FriendID);
+
+            if (existingFriend == null)
+            {
+                return NotFound();
+            }
+
+            existingFriend.FriendName = friend.FriendName;
+            existingFriend.Place = friend.Place;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var existingFriend = _friends
+                .FirstOrDefault(f => f.FriendID == id);
+
+            if (existingFriend == null)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var existingFriendIndex = _friends
+                .FindIndex(f => f.FriendID == id);
+
+            if (existingFriendIndex == -1)
+            {
+                return NotFound();
+            }
+
+            _friends.RemoveAt(existingFriendIndex);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Relocate(int id)
+        {
+            var existingFriend = _friends
+                .FirstOrDefault(f => f.FriendID == id);
+
+            if (existingFriend == null)
+            {
+                return NotFound();
+            }
+
+            return View(new RelocateFriendModel(existingFriend));
+        }
+
+        [HttpPost]
+        public IActionResult Relocate(RelocateFriendModel relocation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(relocation);
+            }
+
+            var existingFriend = _friends
+                .FirstOrDefault(f => f.FriendID == relocation.FriendId);
+
+            if (existingFriend == null)
+            {
+                return NotFound();
+            }
+
+            existingFriend.Place = relocation.Place;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ lesson18_XSS&CORS/anti-CSRF-in-mvc/FriendsManager.MVC/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FriendsManager.MVC.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
