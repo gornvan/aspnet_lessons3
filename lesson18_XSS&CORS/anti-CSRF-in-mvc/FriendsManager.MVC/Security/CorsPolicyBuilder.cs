f70fdd0575ba73eed52a31ab157de837e6c7d23b
gornvan <grobelvan@gmail.com>
2024-10-30 19:07:52 +0100

    Create a policy builder to allow the header MY-MVC-SECURITY-CRUTCH-HEADER and the method POST for it

---
+++ lesson18_XSS&CORS/anti-CSRF-in-mvc/FriendsManager.MVC/Security/CorsPolicyBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace FriendsManager.MVC.Security
+{
+    public static class CorsPolicyBuilder
+    {
+        public static void AddMvcCrutchCorsPolicy(this WebApplicationBuilder builder, string policyName)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(policyName, policy =>
+                {
+                    policy.WithOrigins("http://localhost") // Replace with allowed origin
+                          .WithMethods("POST")
+                          .WithHeaders("my-mvc-security-crutch-header")
+                          .AllowCredentials();
+                });
+            });
+        }
+
+    }
+}
