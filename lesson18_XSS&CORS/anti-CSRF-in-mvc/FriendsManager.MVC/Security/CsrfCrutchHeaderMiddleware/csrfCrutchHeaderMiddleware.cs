8a373c5eee6776b49feecc1f09da0534198a6e10
gornvan <grobelvan@gmail.com>
2024-10-30 19:07:06 +0100

    Create a middleware which checks the presense of the new header, disallowing any requests without it

---
+++ lesson18_XSS&CORS/anti-CSRF-in-mvc/FriendsManager.MVC/Security/CsrfCrutchHeaderMiddleware/csrfCrutchHeaderMiddleware.cs
@@ -0,0 +1,46 @@
+﻿namespace FriendsManager.MVC.Security.CsrfCrutchHeaderMiddleware
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+
+    public class SecurityHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<SecurityHeaderMiddleware> _logger;
+
+        public SecurityHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+            {
+                await _next(context).ConfigureAwait(false);
+                return;
+            }
+
+            // Check if the required header is present
+            if (
+                !context.Request.Headers.TryGetValue(
+                    SecurityConstants.SecurityCrutchHeaderName, out var headerValue)
+                || headerValue != "CRUTCH"
+            )
+            {
+                // Deny the request by returning a 403 Forbidden response
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync(
+                    $"Forbidden: Missing or invalid security header " +
+                    $"'{SecurityConstants.SecurityCrutchHeaderName}'.");
+                await context.Response.CompleteAsync().ConfigureAwait(false);
+                return;
+            }
+
+            // Call the next middleware if header is present and valid
+            await _next(context);
+        }
+    }
+
+}
---
+++ lesson18_XSS&CORS/anti-CSRF-in-mvc/FriendsManager.MVC/Security/SecurityConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace FriendsManager.MVC.Security
+{
+    public static class SecurityConstants
+    {
+        public const string CosrPolicyNameForMvcCrutchPolicy = "MVC-CRUTCH-POLICY";
+        public const string SecurityCrutchHeaderName = "MY-MVC-SECURITY-CRUTCH-HEADER";
+    }
+}
