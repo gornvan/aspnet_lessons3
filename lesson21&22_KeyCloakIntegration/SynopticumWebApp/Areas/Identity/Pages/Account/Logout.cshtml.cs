aeca0d03a70bc3126658a6c4ee586222f781a726
gornvan <grobelvan@gmail.com>
2024-11-18 18:29:46 +0100

    Logout: sign the user out using Cookies scheme

--- lesson21&22_KeyCloakIntegration/SynopticumWebApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ lesson21&22_KeyCloakIntegration/SynopticumWebApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,13 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace SynopticumWebApp.Areas.Identity.Pages.Account
 {
@@ -25,7 +23,8 @@ namespace SynopticumWebApp.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
-            await _signInManager.SignOutAsync();
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
