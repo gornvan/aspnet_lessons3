a5795d13ef35e871074241305fac86d6d55b30ac
gornvan <grobelvan@gmail.com>
2024-11-18 18:35:44 +0100

    Create the User entity and use it as the UserT parameter for DbContext

--- lesson21&22_KeyCloakIntegration/SynopticumWebApp/Data/ApplicationDbContext.cs
+++ lesson21&22_KeyCloakIntegration/SynopticumWebApp/Data/ApplicationDbContext.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SynopticumWebApp.Data.Entities;
 
 namespace SynopticumWebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<SynopticumUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
     }
---
+++ lesson21&22_KeyCloakIntegration/SynopticumWebApp/Data/Entities/SynopticumUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SynopticumCore.Contract.Enums;
+
+namespace SynopticumWebApp.Data.Entities
+{
+    public class SynopticumUser: IdentityUser
+    {
+        public IdentityProvider IdentityProvider { get; set; }
+    }
+}
