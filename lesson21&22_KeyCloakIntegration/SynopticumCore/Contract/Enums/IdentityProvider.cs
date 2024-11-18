b9cabf5b3f86b89a7cbffb5a943c25d7b7c38df9
gornvan <grobelvan@gmail.com>
2024-11-18 18:22:50 +0100

    Create an enum for IdentityProviders

---
+++ lesson21&22_KeyCloakIntegration/SynopticumCore/Contract/Enums/IdentityProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace SynopticumCore.Contract.Enums
+{
+    public enum IdentityProvider
+    {
+        Local = 0, // not used for now
+
+        KeyCloak = 1,
+
+        Google = 2,
+    }
+}
