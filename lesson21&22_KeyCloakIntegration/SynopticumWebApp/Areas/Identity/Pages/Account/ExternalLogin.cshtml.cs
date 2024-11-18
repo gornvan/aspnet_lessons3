22cb270dc501389dee6521d9ed08904c8891f0e4
gornvan <grobelvan@gmail.com>
2024-11-18 18:27:36 +0100

    Rework ExternalLogin page - unify the flow of logging in, relying on the middlewares of KeyCloak and Google

--- lesson21&22_KeyCloakIntegration/SynopticumWebApp/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ lesson21&22_KeyCloakIntegration/SynopticumWebApp/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -2,47 +2,40 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
+using SynopticumCore.Contract.Enums;
+using SynopticumWebApp.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Security.Principal;
 
 namespace SynopticumWebApp.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class ExternalLoginModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly IUserStore<IdentityUser> _userStore;
-        private readonly IUserEmailStore<IdentityUser> _emailStore;
-        private readonly IEmailSender _emailSender;
+        private readonly SignInManager<SynopticumUser> _signInManager;
+        private readonly UserManager<SynopticumUser> _userManager;
+        private readonly IUserStore<SynopticumUser> _userStore;
         private readonly ILogger<ExternalLoginModel> _logger;
 
         public ExternalLoginModel(
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager,
-            IUserStore<IdentityUser> userStore,
-            ILogger<ExternalLoginModel> logger,
-            IEmailSender emailSender)
+            SignInManager<SynopticumUser> signInManager,
+            UserManager<SynopticumUser> userManager,
+            IUserStore<SynopticumUser> userStore,
+            ILogger<ExternalLoginModel> logger)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _userStore = userStore;
-            _emailStore = GetEmailStore();
             _logger = logger;
-            _emailSender = emailSender;
         }
 
         /// <summary>
@@ -93,131 +86,85 @@ namespace SynopticumWebApp.Areas.Identity.Pages.Account
             // Request a redirect to the external login provider.
             var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
             var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-            return new ChallengeResult(provider, properties);
+            var chalRes = new ChallengeResult(provider, properties);
+            return chalRes;
         }
 
         public async Task<IActionResult> OnGetCallbackAsync(string returnUrl = null, string remoteError = null)
         {
             returnUrl ??= Url.Content("~/");
+
             if (remoteError != null)
             {
                 ErrorMessage = $"Error from external provider: {remoteError}";
                 return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
             }
-            var info = await _signInManager.GetExternalLoginInfoAsync();
-            if (info == null)
-            {
-                ErrorMessage = "Error loading external login information.";
-                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
-            }
 
-            // Sign in the user with this external login provider if the user already has a login.
-            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
-            if (result.Succeeded)
+            if(User.Identity?.IsAuthenticated ?? false)
             {
-                _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
+                var ourUser = await CreateUserIfNotKnown(User);
+                await SignTheUserIn(ourUser);
                 return LocalRedirect(returnUrl);
             }
-            if (result.IsLockedOut)
-            {
-                return RedirectToPage("./Lockout");
-            }
-            else
-            {
-                // If the user does not have an account, then ask the user to create an account.
-                ReturnUrl = returnUrl;
-                ProviderDisplayName = info.ProviderDisplayName;
-                if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
-                {
-                    Input = new InputModel
-                    {
-                        Email = info.Principal.FindFirstValue(ClaimTypes.Email)
-                    };
-                }
-                return Page();
-            }
+
+            ErrorMessage = "Failed authenticating with external provider.";
+            return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
         }
 
-        public async Task<IActionResult> OnPostConfirmationAsync(string returnUrl = null)
+        private async Task<ClaimsPrincipal> SignInWithKeyCloak()
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
-            // Get the information about the user from the external login provider
-            var info = await _signInManager.GetExternalLoginInfoAsync();
-            if (info == null)
+            var schema = "KeyCloak";
+            // Authenticate the user with the OpenID Connect scheme
+            var authResult = await this.HttpContext.AuthenticateAsync(schema);
+
+            if (authResult.Succeeded)
             {
-                ErrorMessage = "Error loading external login information during confirmation.";
-                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+                return authResult.Principal;
             }
-
-            if (ModelState.IsValid)
-            {
-                var user = CreateUser();
-
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-
-                var result = await _userManager.CreateAsync(user);
-                if (result.Succeeded)
-                {
-                    result = await _userManager.AddLoginAsync(user, info);
-                    if (result.Succeeded)
-                    {
-                        _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
-
-                        var userId = await _userManager.GetUserIdAsync(user);
-                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                        var callbackUrl = Url.Page(
-                            "/Account/ConfirmEmail",
-                            pageHandler: null,
-                            values: new { area = "Identity", userId = userId, code = code },
-                            protocol: Request.Scheme);
-
-                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                        // If account confirmation is required, we need to show the link if we don't have a real email sender
-                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                        {
-                            return RedirectToPage("./RegisterConfirmation", new { Email = Input.Email });
-                        }
-
-                        await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
-                        return LocalRedirect(returnUrl);
-                    }
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
-
-            ProviderDisplayName = info.ProviderDisplayName;
-            ReturnUrl = returnUrl;
-            return Page();
+            return null;
         }
 
-        private IdentityUser CreateUser()
+        private async Task<SynopticumUser> CreateUserIfNotKnown(ClaimsPrincipal principal)
         {
-            try
+            var provider = GetAuthProvider(principal.Identity);
+            var email = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+            // if exists, return it
+            var existingUser = await _userManager.FindByEmailAsync(email);
+            if(existingUser != null)
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return existingUser;
             }
-            catch
+
+            // otherwise - create it
+            var newUser = new SynopticumUser
             {
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
-                    $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the external login page in /Areas/Identity/Pages/Account/ExternalLogin.cshtml");
-            }
+                UserName = email,
+                Email = email,
+                IdentityProvider = provider
+            };
+
+            var identityResult = await _userManager.CreateAsync(newUser);
+
+            return newUser;
         }
 
-        private IUserEmailStore<IdentityUser> GetEmailStore()
+        private async Task SignTheUserIn(SynopticumUser user)
         {
-            if (!_userManager.SupportsUserEmail)
+            await _signInManager.SignInAsync(
+                user,
+                isPersistent: false,
+                authenticationMethod: CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        private IdentityProvider GetAuthProvider(IIdentity identity)
+        {
+            if(identity.AuthenticationType == "Google")
             {
-                throw new NotSupportedException("The default UI requires a user store with email support.");
+                return IdentityProvider.Google;
             }
-            return (IUserEmailStore<IdentityUser>)_userStore;
+
+            return IdentityProvider.KeyCloak;
         }
     }
 }
