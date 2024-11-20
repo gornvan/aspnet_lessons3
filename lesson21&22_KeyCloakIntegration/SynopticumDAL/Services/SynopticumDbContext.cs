9989e81bd04389b3b5cd2e62ab42606dc58d0c4a
gornvan <grobelvan@gmail.com>
2024-11-20 20:33:56 +0100

    DbContext: call base.OnModelCreating(modelBuilder) to initialize IdentityRole and related Entities, cleanup

--- lesson21&22_KeyCloakIntegration/SynopticumDAL/Services/SynopticumDbContext.cs
+++ lesson21&22_KeyCloakIntegration/SynopticumDAL/Services/SynopticumDbContext.cs
@@ -1,25 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SynopticumWebApp.Data.Entities;
 
 namespace SynopticumDAL.Services;
 
-public partial class SynopticumDbContext : DbContext
+public partial class SynopticumDbContext : IdentityDbContext<SynopticumUser>
 {
     public SynopticumDbContext(DbContextOptions<SynopticumDbContext> options)
         : base(options)
     {
     }
 
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder
             .UseCollation("utf8mb3_bin")
             .HasCharSet("utf8mb3");
 
         modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-
-        OnModelCreatingPartial(modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
