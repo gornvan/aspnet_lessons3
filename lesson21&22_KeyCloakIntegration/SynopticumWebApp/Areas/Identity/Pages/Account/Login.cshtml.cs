57e6667decbba1103025fa4149e65e5605cb5518
gornvan <grobelvan@gmail.com>
2024-11-20 17:21:55 +0100

    fix post-login redirect to '/'

--- lesson21&22_KeyCloakIntegration/SynopticumWebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ lesson21&22_KeyCloakIntegration/SynopticumWebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -48,7 +48,7 @@ namespace SynopticumWebApp.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
