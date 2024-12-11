2236fc7b391dab061a14592690b9a34dbbe190cf
gornvan <grobelvan@gmail.com>
2024-12-11 20:23:23 +0100

    Create a class for settings of token validation

---
+++ lesson21&22_KeyCloakIntegration/SynopticumWebAPI/ConfigurationSections/TokenValidationParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace SynopticumWebAPI.ConfigurationSections
+{
+    public class TokenValidationParametersConfig
+    {
+        public string Authority { get; set; }
+        public bool ValidateIssuer { get; set; }
+        public string ValidIssuer { get; set; }
+        public bool ValidateAudience { get; set; }
+        public string ValidAudience { get; set; }
+        public bool ValidateLifetime { get; set; }
+        public bool ValidateIssuerSigningKey { get; set; }
+    }
+}
